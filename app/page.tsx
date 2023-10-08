09ef7019cf00d9a96bbc5f2cdf9cf29bd04c7841
Jesper Kuutti <jesper.kuutti@gmail.com>
2023-10-08 15:18:30 +0300

    Move to NextJS

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main className="text-red flex justify-center">
+      <h1 className="text-orange-500">Spooky halloween 🎃</h1>
+    </main>
+  )
+}
