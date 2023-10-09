5c9426b369e17de64dc674493fd513ba21cfc7f5
Jesper Kuutti <jesper.kuutti@gmail.com>
2023-10-09 14:56:55 +0300

    Moved information behind authentication

--- app/details/page.tsx
+++ app/details/page.tsx
@@ -1,5 +1,15 @@
 export default function DetailsPage() {
     return <main>
+        <p>Suuri Halloween-juhla. Katsotaan valittuja elokuvia. Mahdollisesti jokainen päättää yhden.
+        </p>
+        <p>Voidaan myös pukeutua Halloween-teemaisesti ja käydä Kauhusaunassa.</p>
+        <p>
+            Tässä linkki Spotify-listaan: TODO
+        </p>
+
+        <p>
+            Lisää tapahtuma kalenteriin: TODO
+        </p>
         <p>Tapahtumapaikka: XXXX, Espoo. TODO: lisää kartta.</p>
         <p>Aikataulu: XXX </p>
     </main>
--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,7 @@ export default function Home() {
   return (
     <main className="max-w-xl mx-auto">
       <h1 className="text-orange-500 text-center">Spooky halloween 🎃</h1>
-      <p>Suuri Halloween-juhla. Katsotaan valittuja elokuvia. Mahdollisesti jokainen päättää yhden.
-      </p>
-      <p>Voidaan myös pukeutua Halloween-teemaisesti ja käydä Kauhusaunassa.</p>
-      <p>
-        Tässä linkki Spotify-listaan: TODO
-      </p>
 
-      <p>
-        Lisää tapahtuma kalenteriin: TODO
-      </p>
     </main>
   )
 }
