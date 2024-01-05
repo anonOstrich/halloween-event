c6efac5c153e7f29a543f6b0b7a79f20857f961d
Jesper Kuutti <jesper.kuutti@gmail.com>
2024-01-05 13:34:52 +0200

    UI improvements to the list of movie suggestions

--- components/MovieApiSearch.tsx
+++ components/MovieApiSearch.tsx
@@ -13,6 +13,8 @@ interface MovieApiSearchProps {
 export default function MovieApiSearch({completeMovieInformationCallBack}: MovieApiSearchProps) {
     const [searchTerm, setSearchTerm] = useState('')
     const [possibleMovies, setPossibleMovies] = useState<Array<Movie>>([])
+    // It's nicer to hide but keep them 'open' in certain situations (examining the filled information)
+    const [possibleMoviesVisible, setPossibleMoviesVisible] = useState(true)
     //TODO:  NodeJS is probably not the exact type I'm looking for, if this is run on the client...
     const [pollTimeout, setPollTimeout] = useState<NodeJS.Timeout | null>(null);
 
@@ -53,26 +55,36 @@ export default function MovieApiSearch({completeMovieInformationCallBack}: Movie
         const movie = possibleMovies.find(m => m.id === id);
         if (movie == null) return;
         const year = (new Date(movie.release_date)).getFullYear()
+        setPossibleMoviesVisible(false)
         completeMovieInformationCallBack(movie.title, year, movie.overview)
     }
 
 
-    return <div className="py-2 px-2 m-10 mb-20 border-black flex flex-col items-center justify-stretch">
+    return <div className=" py-2 px-2 m-10 mb-20 border-black flex flex-col items-center justify-stretch">
         <input
             className="p-5 w-full"
          type="text" placeholder="Search for the movie by name!"
          value={searchTerm}
         onChange={handleSearchTermChange}
+        onFocus={() => {setPossibleMoviesVisible(true)}}
          />
-
-         {possibleMovies.length > 0 && (
-            <ul>
+        
+        {
+            /**
+             * TODO: Make a scrollable list
+             */
+        }
+        <div className={`relative w-full ${possibleMoviesVisible ? '' : 'hidden'}`}>
+        {possibleMovies.length > 0 && (
+            <ul className="absolute text-black bg-white black top-100%">
                 {possibleMovies.map(movie => (
                 <li key={movie.id}>
                     <SearchResult title={movie.title} id={movie.id} releaseDate={movie.release_date} handleClick={fillInMovieInformation} />
                 </li>))}
             </ul>
          )}
+        </div>
+
     </div>
 }
 
@@ -88,7 +100,7 @@ function SearchResult({title, releaseDate, id, handleClick}: SearchResultProps) 
 
 
     return <div
-        className="px-3 py-2 border-red-600 border-2 cursor-pointer"
+        className="px-3 py-2 border-red-600 border-2 cursor-pointer hover:bg-gray-200"
     onClick={() => handleClick(id)}>
         <p>{title} ({releaseDate})</p>
     </div>
