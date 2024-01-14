12914912c13922db52f05bd4999dad32acb1783d
Jesper Kuutti <jesper.kuutti@gmail.com>
2024-01-15 01:45:32 +0200

    Polished movies page

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -5,7 +5,7 @@ export default function MovieCard({ movie }: { movie: Movie }) {
     const { id, title, year, description } = movie
 
     return <Link href={`/movies/${id}`} className="block h-full">
-        <article className="px-4 py-3 bg-slate-400 rounded border-2 hover:bg-slate-500 transition-all flex gap-6 justify-between items-center h-full">
+        <article className="px-4 py-3 bg-slate-400 rounded shadow-md shadow-gray-600 hover:bg-slate-500 transition-all flex gap-6 justify-between items-center h-full">
             <div>
                 <h5>{title}</h5>
                 <h6>{year}</h6>
