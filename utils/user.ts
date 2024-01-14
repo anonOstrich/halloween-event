448f6e91702e4465c924b4b6d710a052966af9dd
Jesper Kuutti <jesper.kuutti@gmail.com>
2024-01-15 01:18:11 +0200

    logged in in non-auth environments

--- utils/user.ts
+++ utils/user.ts
@@ -2,6 +2,7 @@ import { auth } from "@clerk/nextjs";
 
 //TODO: Should I also check that the user is in my DB?
 export function isLoggedIn() {
-    const {userId} = auth()
+    if (process.env["DISABLE_AUTH"] == "true") { return true }
+    const { userId } = auth()
     return userId != null
 }
