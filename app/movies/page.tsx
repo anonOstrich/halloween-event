4c0e0e03563d4c0e4c2d02dc9ebc067cc2db26df
Jesper Kuutti <jesper.kuutti@gmail.com>
2024-01-14 20:47:18 +0200

    Extracting components, aligning layouts

--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -6,16 +6,15 @@ import Link from "next/link"
 
 export default async function Movies() {
 
-    const movies = await prisma.movie.findMany({
-        take: 10
-    })
+    //TODO: if many many movies, don't load them all at once
+    const movies = await prisma.movie.findMany({})
 
-    const moviesComponent = movies.length == 0 ? <h3>No movies :/</h3> : <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
-        {movies.map(m => <li><MovieCard movie={m} /></li>)}
+    const moviesComponent = movies.length == 0 ? <h3>No movies :/</h3> : <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 items-stretch">
+        {movies.map(m => <li className="h-full" key={m.id}><MovieCard movie={m} /></li>)}
     </ul>
 
-    return <section>
-        <Link href="/movies/add-movie"><button className="button">Add a new movie 🎞️</button></Link>
+    return <section className="m-auto max-w-4xl flex flex-col items-center justify-between gap-5">
+        <Link href="/movies/add-movie" className="border-2 border-white p-5 rounded-md hover:bg-gray-700">Add a new movie 🎞️</Link>
         {moviesComponent}
     </section>
 }
